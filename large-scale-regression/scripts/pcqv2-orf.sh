1a8c9e34d99a223d603c1e3daaae965ccefe1937
debarati <das00015@umn.edu>
2023-04-27 19:40:44 -0500

    add launch.json for debugging, change orf files to reflect changes to avoid cuda errors

--- large-scale-regression/scripts/pcqv2-orf.sh
+++ large-scale-regression/scripts/pcqv2-orf.sh
@@ -6,7 +6,7 @@ fairseq-train \
 --user-dir ../tokengt \
 --num-workers 16 \
 --ddp-backend=legacy_ddp \
---dataset-name pcqm4mv2 \
+--dataset-name upfd \
 --dataset-source ogb \
 --task graph_prediction \
 --criterion l1_loss \
@@ -15,14 +15,18 @@ fairseq-train \
 --orf-node-id-dim 64 \
 --stochastic-depth \
 --prenorm \
---num-classes 1 \
+--num-classes 2 \
 --attention-dropout 0.1 --act-dropout 0.1 --dropout 0.0 \
 --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 5.0 --weight-decay 0.1 \
 --lr-scheduler polynomial_decay --power 1 --warmup-updates 60000 --total-num-update 1000000 \
 --lr 2e-4 --end-learning-rate 1e-9 \
---batch-size 128 \
---fp16 \
+--batch-size 32 \
 --data-buffer-size 20 \
---save-dir ./ckpts/pcqv2-tokengt-orf64 \
---tensorboard-logdir ./tb/pcqv2-tokengt-orf64 \
+--save-dir ./ckpts/upfd \
+--tensorboard-logdir ./tb/upfd \
 --no-epoch-checkpoints
+
+#--num-classes 1 \
+#--fp16 \
+#--batch-size 128 \
+#--dataset-name pcqm4mv2 \
