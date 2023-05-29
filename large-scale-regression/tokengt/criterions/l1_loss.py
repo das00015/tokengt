7c5e487ec552fe38ba8b2b9b8d20a991c6229cc7
debarati <das00015@umn.edu>
2023-05-29 01:10:32 -0500

    l1 loss

--- large-scale-regression/tokengt/criterions/l1_loss.py
+++ large-scale-regression/tokengt/criterions/l1_loss.py
@@ -13,7 +13,7 @@ from fairseq.criterions import FairseqCriterion, register_criterion
 @register_criterion("l1_loss", dataclass=FairseqDataclass)
 class GraphPredictionL1Loss(FairseqCriterion):
     """
-    Implementation for the L1 loss (MAE loss) used in tokengt model training.
+    Implementation for the L1 loss (MAE loss) used in tokengt model training --> changed this to BCE!
     """
 
     def forward(self, model, sample, reduce=True):
@@ -24,26 +24,23 @@ class GraphPredictionL1Loss(FairseqCriterion):
         2) the sample size, which is used as the denominator for the gradient
         3) logging outputs to display while training
         """
-        sample_size = sample["nsamples"]
+        sample_size = sample["nsamples"] # total no of samples in the batch.
 
         with torch.no_grad():
             natoms = max(sample["net_input"]["batched_data"]["node_num"])
-
+    
         # model's forward method unpacks the input data in the dictionary and assigns output of forward to logits
         logits = model(**sample["net_input"])
 
         # for each sample in the batch; get the logits for the sample. get targets returns the target for each sample.
         targets = model.get_targets(sample, [logits])
-
-        # if the model is a binary classifier, the logits could be a single scalar value representing the probability of the positive class, 
-        # before being transformed to a probability using a sigmoid activation function.
-
-        #loss_fn = nn.BCEWithLogitsLoss()
-        #loss = loss_fn(logits, targets.float())
-    
+        #new_targets = sample["net_input"]["batched_data"]["y"].view(-1,1).float() # we get it directly from the sample. 
+        
         # this is done to match the size of the logits tensor along the batch dimension.
         new_targets = targets[: logits.size(0)].float()
-        loss = nn.BCEWithLogitsLoss(reduction="mean")(logits, new_targets) 
+        assert (sample["net_input"]["batched_data"]["y"].view(-1,1).float() == new_targets).all()
+        assert (max(sample['net_input']['batched_data']['node_num'])<512)
+        loss = nn.BCEWithLogitsLoss(reduction="sum")(logits, new_targets) 
         #loss = nn.L1Loss(reduction="sum")(logits, targets[: logits.size(0)]) # change this to cross entropy
 
         logging_output = {
@@ -52,16 +49,19 @@ class GraphPredictionL1Loss(FairseqCriterion):
             "nsentences": sample_size,
             "ntokens": natoms,
         }
-        return loss, sample_size, logging_output
+        #print("Loss calculated for an actual sample of",logging_output['sample_size']," :", loss)
+        return loss, sample_size, logging_output # need to explore why sample_size different from logging_output[sample_size]
 
     @staticmethod
     def reduce_metrics(logging_outputs) -> None:
         """Aggregate logging outputs from data parallel training."""
         loss_sum = sum(log.get("loss", 0) for log in logging_outputs)
-        
+        #print("loss sum --> ", loss_sum)
         sample_size = sum(log.get("sample_size", 0) for log in logging_outputs)
-        print("sample size -->", sample_size)
+        #print("sample size -->", sample_size)
+
         metrics.log_scalar("loss", loss_sum / sample_size, sample_size, round=6)
+        #metrics.log_scalar("loss", loss_sum)
 
     @staticmethod
     def logging_outputs_can_be_summed() -> bool:
